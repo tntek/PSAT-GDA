89647ee66692da02359be7ca240b96b5cdbab19f
TNTekLab <steventangsong@gmail.com>
2022-01-31 22:53:21 +0800

    Add files via upload

--- run_target_PSAT-GDA.sh
+++ run_target_PSAT-GDA.sh
@@ -1,26 +1,26 @@
 # ########R2-------------------------------------
-~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset VISDA-C --gpu_id 0 --s 0 --t 1 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/ --lr 1e-3;
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset VISDA-C --gpu_id 0 --s 0 --t 1 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/ --lr 1e-3;
 
-# ~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office --gpu_id 1 --s 0 --t 1 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/ --seed 2020
-# ~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office --gpu_id 0 --s 0 --t 2 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/ --seed 2020
-# ~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office --gpu_id 0 --s 1 --t 0 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/ --seed 2020
-# ~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office --gpu_id 0 --s 1 --t 2 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/ --seed 2020
-# ~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office --gpu_id 0 --s 2 --t 0 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/ --seed 2020
-# ~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office --gpu_id 0 --s 2 --t 1 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/ --seed 2020
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office --gpu_id 1 --s 0 --t 1 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/ --seed 2020
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office --gpu_id 0 --s 0 --t 2 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/ --seed 2020
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office --gpu_id 0 --s 1 --t 0 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/ --seed 2020
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office --gpu_id 0 --s 1 --t 2 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/ --seed 2020
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office --gpu_id 0 --s 2 --t 0 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/ --seed 2020
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office --gpu_id 0 --s 2 --t 1 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/ --seed 2020
 
-# ~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 0 --t 1 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/;
-~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 0 --t 2 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/;
-~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 0 --t 3 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/;
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 0 --t 1 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/;
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 0 --t 2 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/;
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 0 --t 3 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/;
 
-~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 1 --t 0 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/;
-~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 1 --t 2 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/;
-~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 1 --t 3 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/;
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 1 --t 0 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/;
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 1 --t 2 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/;
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 1 --t 3 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/;
 
-~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 2 --t 0 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/;
-~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 2 --t 1 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/;
-~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 2 --t 3 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/;
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 2 --t 0 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/;
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 2 --t 1 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/;
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 2 --t 3 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/;
 
-~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 3 --t 0 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/;
-~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 3 --t 1 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/;
-~/anaconda3/envs/vit_kd/bin/python target_PDT.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 3 --t 2 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PDT_08_0112/;
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 3 --t 0 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/;
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 3 --t 1 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/;
+~/anaconda3/envs/vit_kd/bin/python target_PSAT-GDA.py --cls_par 0.6 --da uda --dset office-home --gpu_id 0 --s 3 --t 2 --output_src ckpskdr2/source_tt_08/ --output ckpskdr2/target_PSAT-GDA_08_0112/;
 
